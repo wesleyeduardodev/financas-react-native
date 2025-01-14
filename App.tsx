fd2d972b22786171710f25980b964e842c118005
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2025-01-14 13:35:00 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {NavigationContainer} from "@react-navigation/native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import { Home } from "./src/screens/components/Home";
-import { SubCategoriesScreen } from "./src/screens/components/SubCategoriesScreen";
-import { FinancialSummary } from "./src/screens/components/FinancialSummary"; // Importando o resumo financeiro
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import {Home} from "./src/screens/components/Home";
+import {SubCategoriesScreen} from "./src/screens/components/SubCategoriesScreen";
+import {CategoryScreen} from "./src/screens/components/CategoryScreen";
+import {FinancialSummary} from "./src/screens/components/FinancialSummary"; // Importando o resumo financeiro
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
 
 const Drawer = createDrawerNavigator();
 const Tab = createMaterialTopTabNavigator();
@@ -15,17 +16,17 @@ function Tabs() {
         <Tab.Navigator
             initialRouteName="Expenses"
             screenOptions={{
-                tabBarStyle: { backgroundColor: "#4CAF50" },
+                tabBarStyle: {backgroundColor: "#4CAF50"},
                 tabBarActiveTintColor: "#FFF",
-                tabBarIndicatorStyle: { backgroundColor: "#FFF" },
+                tabBarIndicatorStyle: {backgroundColor: "#FFF"},
             }}
         >
             <Tab.Screen
                 name="Expenses"
                 component={Home}
                 options={{
-                    tabBarIcon: ({ color }) => (
-                        <Icon name="attach-money" size={20} color={color} />
+                    tabBarIcon: ({color}) => (
+                        <Icon name="attach-money" size={20} color={color}/>
                     ),
                     tabBarLabel: "Registro Financeiro",
                 }}
@@ -34,8 +35,8 @@ function Tabs() {
                 name="Categories"
                 component={FinancialSummary} // Alterado para exibir o resumo financeiro
                 options={{
-                    tabBarIcon: ({ color }) => (
-                        <Icon name="category" size={20} color={color} />
+                    tabBarIcon: ({color}) => (
+                        <Icon name="category" size={20} color={color}/>
                     ),
                     tabBarLabel: "Resumo Financeiro", // Rótulo ajustado
                 }}
@@ -44,8 +45,8 @@ function Tabs() {
                 name="Subcategories"
                 component={SubCategoriesScreen}
                 options={{
-                    tabBarIcon: ({ color }) => (
-                        <Icon name="subdirectory-arrow-right" size={20} color={color} />
+                    tabBarIcon: ({color}) => (
+                        <Icon name="subdirectory-arrow-right" size={20} color={color}/>
                     ),
                     tabBarLabel: "Subcategorias",
                 }}
@@ -59,8 +60,8 @@ export default function App() {
         <NavigationContainer>
             <Drawer.Navigator
                 screenOptions={{
-                    drawerStyle: { backgroundColor: "#f4f4f4", width: 240 },
-                    headerStyle: { backgroundColor: "#4CAF50" },
+                    drawerStyle: {backgroundColor: "#f4f4f4", width: 240},
+                    headerStyle: {backgroundColor: "#4CAF50"},
                     headerTintColor: "#FFF",
                 }}
             >
@@ -68,18 +69,29 @@ export default function App() {
                     name="Home"
                     component={Tabs}
                     options={{
-                        drawerIcon: ({ color }) => (
-                            <Icon name="home" size={20} color={color} />
+                        drawerIcon: ({color}) => (
+                            <Icon name="home" size={20} color={color}/>
                         ),
                         title: "Início",
                     }}
                 />
                 <Drawer.Screen
+                    name="Categories"
+                    component={CategoryScreen}
+                    options={{
+                        drawerIcon: ({color}) => (
+                            <Icon name="category" size={20} color={color}/>
+                        ),
+                        title: "Categorias",
+                    }}
+                />
+
+                <Drawer.Screen
                     name="Subcategories"
                     component={SubCategoriesScreen}
                     options={{
-                        drawerIcon: ({ color }) => (
-                            <Icon name="subdirectory-arrow-right" size={20} color={color} />
+                        drawerIcon: ({color}) => (
+                            <Icon name="subdirectory-arrow-right" size={20} color={color}/>
                         ),
                         title: "Subcategorias",
                     }}
